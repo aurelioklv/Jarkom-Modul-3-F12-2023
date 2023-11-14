1364a95eeb8b1ca7d875449fdf0a0eaa7ad62ddc
Aurelio Killian Lexi Verrill <87407047+aurelioklv@users.noreply.github.com>
2023-11-14 15:50:21 +0000

    add no 6

--- scripts/f12_linie.sh
+++ scripts/f12_linie.sh
@@ -26,3 +26,70 @@ else
   echo -e "${BG_RED}Error updating resolv.conf.${RESET}"
   exit 1
 fi
+
+
+# Install requirements using apt-get
+echo -e "${BG_BLUE}Installing requirements...${RESET}"
+apt-get update
+apt-get install -y wget unzip php nginx
+
+if [ $? -eq 0 ]; then
+  echo -e "${BG_GREEN}Requirements installed successfully.${RESET}"
+else
+  echo -e "${BG_RED}Error installing requirements.${RESET}"
+  exit 1
+fi
+
+
+# Downloading file
+echo -e "${BG_BLUE}Download and unziping...${RESET}"
+wget -O 'granz.channel.yyy.com.zip' 'https://drive.usercontent.google.com/download?id=1ViSkRq7SmwZgdK64eRbr5Fm1EGCTPrU1'
+unzip granz.channel.yyy.com.zip
+mv modul-3/* /var/www/html
+echo -e "${BG_GREEN}File moved to /var/www/html${RESET}"
+
+
+# Configuring nginx
+# /etc/nginx/sites-available/default
+file_to_modify="/etc/nginx/sites-available/default"
+echo -e "${BG_BLUE}Configuring $file_to_modify${RESET}"
+echo 'server {
+
+listen 80;
+
+root /var/www/html;
+
+index index.php index.html index.htm;
+server_name _;
+
+location / {
+        try_files $uri $uri/ /index.php?$query_string;
+}
+
+# pass PHP scripts to FastCGI server
+location ~ \.php$ {
+        include snippets/fastcgi-php.conf;
+        fastcgi_pass unix:/run/php/php7.0-fpm.sock;
+}
+
+location ~ /\.ht {
+        deny all;
+}
+
+error_log /var/log/nginx/jarkom_error.log;
+access_log /var/log/nginx/jarkom_access.log;
+}' > "$file_to_modify"
+
+if [ $? -eq 0 ]; then
+  echo -e "${BG_GREEN}$file_to_modify configured successfully.${RESET}"
+else
+  echo -e "${BG_RED}Error configuring $file_to_modify.${RESET}"
+fi
+
+
+# Restarting services
+echo -e "${BG_CYAN}Restarting services ...${RESET}"
+service php7.0-fpm start
+service nginx restart
+
+echo -e "${BG_MAGENTA}DONE${RESET}"
