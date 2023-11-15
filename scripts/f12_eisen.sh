e6cb59feecb94f66fbba429b67a3becdb8bc7547
Aurelio Killian Lexi Verrill <87407047+aurelioklv@users.noreply.github.com>
2023-11-15 12:56:37 +0000

    add no 7-9

--- scripts/f12_eisen.sh
+++ scripts/f12_eisen.sh
@@ -17,7 +17,8 @@ RESET="\e[0m"
 # Update resolv.conf
 echo -e "${BG_BLUE}Updating resolv.conf...${RESET}"
 
-echo 'nameserver 192.168.122.1
+echo 'nameserver 192.227.1.2
+nameserver 192.168.122.1
 ' > /etc/resolv.conf
 
 if [ $? -eq 0 ]; then
@@ -26,3 +27,83 @@ else
   echo -e "${BG_RED}Error updating resolv.conf.${RESET}"
   exit 1
 fi
+
+
+# Install requirements using apt-get
+echo -e "${BG_BLUE}Installing requirements...${RESET}"
+apt-get update
+apt-get install -y nginx apache2-utils
+
+if [ $? -eq 0 ]; then
+  echo -e "${BG_GREEN}Requirements installed successfully.${RESET}"
+else
+  echo -e "${BG_RED}Error installing requirements.${RESET}"
+  exit 1
+fi
+
+
+# Configuring nginx
+# /etc/nginx/sites-available/lb-jarkom
+file_to_modify="/etc/nginx/sites-available/lb-jarkom"
+echo -e "${BG_BLUE}Configuring $file_to_modify${RESET}"
+echo 'upstream backend  {
+server 192.227.3.1; #IP Lugner
+server 192.227.3.2; #IP Linie
+server 192.227.3.3; #IP Lawine
+}
+
+server {
+listen 80;
+server_name granz.channel.f12.com;
+
+        location / {
+                proxy_pass http://backend;
+                proxy_set_header    X-Real-IP $remote_addr;
+                proxy_set_header    X-Forwarded-For $proxy_add_x_forwarded_for;
+                proxy_set_header    Host $http_host;
+        }
+
+error_log /var/log/nginx/lb_error.log;
+access_log /var/log/nginx/lb_access.log;
+
+}' > "$file_to_modify"
+
+if [ $? -eq 0 ]; then
+  echo -e "${BG_GREEN}$file_to_modify configured successfully.${RESET}"
+else
+  echo -e "${BG_RED}Error configuring $file_to_modify.${RESET}"
+fi
+
+
+# Unlink default config
+echo -e "${BG_BLUE}Unlink defualt Nginx config ...${RESET}"
+unlink /etc/nginx/sites-enabled/default
+ln -s /etc/nginx/sites-available/lb-jarkom /etc/nginx/sites-enabled/
+echo -e "${BG_GREEN}Unlink successful${RESET}"
+
+
+# Making benchmark output directory
+echo -e "${BG_BLUE}Making benchmark output directory at ~/benchmark ...${RESET}"
+mkdir -p ~/benchmark
+echo -e "${BG_GREEN}Success. Please use -g ~/benchmark/<filename> for apache benchmark${RESET}"
+
+
+# Restarting services
+echo -e "${BG_BLUE}Restarting services ...${RESET}"
+service nginx restart
+
+echo -e "${BG_MAGENTA}DONE${RESET}"
+
+echo "
+
+ ________       _____     _______     
+|\  _____\     / __  \   /  ___  \    
+\ \  \__/     |\/_|\  \ /__/|_/  /|   
+ \ \   __\    \|/ \ \  \|__|//  / /   
+  \ \  \_|         \ \  \   /  /_/__  
+   \ \__\           \ \__\ |\________\\
+    \|__|            \|__|  \|_______|
+                                      
+                                      
+                                      
+"
